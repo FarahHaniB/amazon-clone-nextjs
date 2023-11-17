3ec30e060e231a9b6e4e84bb1a230b285485eebb
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2023-11-17 14:32:58 +0800

    Correction

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,7 +1,8 @@
-const stripe = require("stripe")(process.env.STRIP_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
   const { items, email } = req.body;
+  console.log(req.body)
 
   const transformedItems = items.map((item) => ({
     description: item.description,
@@ -16,21 +17,26 @@ export default async (req, res) => {
     },
   }));
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    shipping_rates: ['shr_1OD14EIyZ061cYngBeaNmsJt'],
-    shipping_address_collection: {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      shipping_rates: ["shr_1OD14EIyZ061cYngBeaNmsJt"],
+      shipping_address_collection: {
         allowed_countries: ["MY", "JP"],
-    },
-    line_items: transformedItems,
-    mode: "payment",
-    success_url: `${process.env.HOST}/success`,
-    cancel_url: `${process.env.HOST}/chekcout`,
-    metadata: {
-      email,
-      images: JSON.stringify(items.map((item) => item.image)),
-    },
-  });
+      },
+      line_items: transformedItems,
+      mode: "payment",
+      success_url: `${process.env.HOST}/success`,
+      cancel_url: `${process.env.HOST}/checkout`,
+      metadata: {
+        email,
+        images: JSON.stringify(items.map((item) => item.image)),
+      },
+    });
 
-  res.status(200).json({ id: session.id})
+    res.status(200).json({ id: session.id });
+  } catch (error) {
+    console.error("Stripe API Error:", error);
+    res.status(500).json({ error: "Failed to create checkout session" });
+  }
 };
